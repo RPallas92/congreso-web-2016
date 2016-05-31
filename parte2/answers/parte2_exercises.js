a9278f4d0a1f2af28d497eaeb465a13f1393e213
rpallas <rpallas@hiberus.com>
2016-05-31 12:06:39 +0200

    Parte2 answers and tests

--- parte2/answers/parte2_exercises.js
+++ parte2/answers/parte2_exercises.js
@@ -3,6 +3,101 @@ var R = require('ramda');
 // Example Data
 var programmingLanguages = ["Haskell", "Elm", "PureScript", "Java", "Scala"];
 
+var newReleases = [
+      {
+        "id": 70111470,
+        "title": "Die Hard",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 4.0,
+        "bookmark": []
+      },
+      {
+        "id": 654356453,
+        "title": "Bad Boys",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 5.0,
+        "bookmark": [{ id:432534, time:65876586 }]
+      },
+      {
+        "id": 65432445,
+        "title": "The Chamber",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 4.0,
+        "bookmark": []
+      },
+      {
+        "id": 675465,
+        "title": "Fracture",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 5.0,
+        "bookmark": [{ id:432534, time:65876586 }]
+      }
+    ];
+
+
+var movieLists = [
+      {
+        name: "Instant Queue",
+        videos : [
+          {
+            "id": 70111470,
+            "title": "Die Hard",
+            "boxarts": [
+              { width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+              { width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+            ],
+            "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+            "rating": 4.0,
+            "bookmark": []
+          },
+          {
+            "id": 654356453,
+            "title": "Bad Boys",
+            "boxarts": [
+              { width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+              { width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" }
+
+            ],
+            "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+            "rating": 5.0,
+            "bookmark": [{ id:432534, time:65876586 }]
+          }
+        ]
+      },
+      {
+        name: "New Releases",
+        videos: [
+          {
+            "id": 65432445,
+            "title": "The Chamber",
+            "boxarts": [
+              { width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+              { width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+            ],
+            "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+            "rating": 4.0,
+            "bookmark": []
+          },
+          {
+            "id": 675465,
+            "title": "Fracture",
+            "boxarts": [
+              { width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+              { width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+              { width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+            ],
+            "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+            "rating": 5.0,
+            "bookmark": [{ id:432534, time:65876586 }]
+          }
+        ]
+      }
+    ];
+
 
 // Exercise 1:
 // ============
@@ -13,8 +108,95 @@ var exercise1 = function(programmingLanguages){
   return allLanguages 
 } 
 
+//Exercise 2
+//====================
+// Proyecta {id, title} en videoAndTitlePairs y devuelvelo
+var exercise2 = function(){
+  var videoAndTitlePairs = [];
+  newReleases.forEach(release => (videoAndTitlePairs.push({id: release.id, title: release.title})));
+  return videoAndTitlePairs;
+    
+}
+
+//Exercise 3
+//====================
+// Lo mismo que el ejercicio 2, utilizando map
+var exercise3 = function(){
+  return  newReleases.map(release => ({id: release.id, title: release.title}));
+}
+
+//Exercise 4:
+//=============
+// Filtra los nuevos lanzamientos cuyo rating sea mayor que 4 y devuelvelos
+var exercise4 = function(){
+  return newReleases.filter(release => release.rating > 4);
+}
+
+//Exercise 5:
+//=============
+// Encadena las funciones filter y map para seleccionar el id y el title de todos los videos con rating 5 y devuelve el resultado
+var exercise5 = function(){
+  return newReleases
+    .filter(release => release.rating === 5.0)
+    .map(release => ( {id: release.id, title: release.title} ))
+}
+
+//Exercise 6: 
+//================
+// Implementa concatAll
+
+Array.prototype.concatAll = function() {
+  var results = [];
+  this.forEach(function(subArray) {
+    results.push.apply(results, subArray);
+  });
+
+  return results;
+};
+
+
+//Exercise 7
+//===============
+//  Utiliza map y concatAll para aplanar movieLists en una lista de ids de video
+
+var exercise7 = function(){ 
+  return movieLists.
+    map(movieList => movieList.videos.
+       map(video => video.id))
+    .concatAll()
+}
+
+
+//Exercise 8
+//===============
+// Utiliza una o más meves map, concatAll y filter para crear un array con los siguientes ítems a partir de movieLists
+// [
+//     {"id": 675465,"title": "Fracture","boxart":"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+//     {"id": 65432445,"title": "The Chamber","boxart":"http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+//     {"id": 654356453,"title": "Bad Boys","boxart":"http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
+//     {"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
+// ];
+var exercise8 = function(){ 
+  return movieLists
+    .map(movieList => movieList.videos
+      .map(video => video.boxarts
+           .filter(boxart => boxart.width === 150)
+           .map(boxart => ({id: video.id, title: video.title, boxart: boxart.url}))
+       ).concatAll()
+    ).concatAll()
+}
+
+
+
+
 
 
 module.exports = { programmingLanguages: programmingLanguages,
-                   exercise1: exercise1
+                   exercise1: exercise1,
+                   exercise2: exercise2,
+                   exercise3: exercise3,
+                   exercise4: exercise4,
+                   exercise5: exercise5,
+                   exercise7: exercise7,
+                   exercise8: exercise8
                  };
--- parte2/answers/parte2_exercises_spec.js
+++ parte2/answers/parte2_exercises_spec.js
@@ -2,10 +2,92 @@ var E = require('./parte2_exercises');
 var assert = require("chai").assert;
 
 describe("Parte 2 Exercises", function(){  
-  var programmingLanguages = E.programmingLanguages;
 
+  //DATA
+  var programmingLanguages = E.programmingLanguages;
+  var exercise2Result = [
+      {
+        "id": 70111470,
+        "title": "Die Hard",
+      },
+      {
+        "id": 654356453,
+        "title": "Bad Boys",
+      },
+      {
+        "id": 65432445,
+        "title": "The Chamber",
+      },
+      {
+        "id": 675465,
+        "title": "Fracture",
+      }
+    ];
+
+    var exercise4Result = [
+      {
+        "id": 654356453,
+        "title": "Bad Boys",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 5.0,
+        "bookmark": [{ id:432534, time:65876586 }]
+      },
+      {
+        "id": 675465,
+        "title": "Fracture",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 5.0,
+        "bookmark": [{ id:432534, time:65876586 }]
+      }
+    ];
+
+    var exercise5Result = [
+      {
+        "id": 654356453,
+        "title": "Bad Boys"
+      },
+      {
+        "id": 675465,
+        "title": "Fracture"
+      }
+    ];
+
+
+    var exercise7Result = [70111470, 654356453, 65432445, 675465];
+      
+    var exercise8Result = [
+        {"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+        {"id": 654356453,"title": "Bad Boys","boxart":"http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
+        {"id": 65432445,"title": "The Chamber","boxart":"http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+        {"id": 675465,"title": "Fracture","boxart":"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" }
+    ];
+
+
+  //TESTS
   it('Exercise 1', function(){
     assert.deepEqual(E.exercise1(programmingLanguages), programmingLanguages);
+  });
+
+  it('Exercise 2', function(){
+    assert.deepEqual(E.exercise2(), exercise2Result);
+  });
+
+  it('Exercise 3', function(){
+    assert.deepEqual(E.exercise3(), exercise2Result);
+  });
+  it('Exercise 4', function(){
+    assert.deepEqual(E.exercise4(), exercise4Result);
+  });
+  it('Exercise 5', function(){
+    assert.deepEqual(E.exercise5(), exercise5Result);
+  });
+  it('Exercise 7', function(){
+    assert.deepEqual(E.exercise7(), exercise7Result);
+  });
+  it('Exercise 8', function(){
+    assert.deepEqual(E.exercise8(), exercise8Result);
   });
 
 });
